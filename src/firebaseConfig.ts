f6bd66e96f82529a74ab29f75536642e10cc9b9a
Anupam kumar Pandit <anupampandit699@gmail.com>
2025-07-13 10:21:12 +0530

    hide firebase credential

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDZ6kMGB3VFahhP9Dt0WhTWgxF85LHrRAc",
-  authDomain: "nambapath-16c7b.firebaseapp.com",
-  projectId: "nambapath-16c7b",
-  storageBucket: "nambapath-16c7b.appspot.com",
-  messagingSenderId: "1075844318135",
+  apiKey: "xyz",
+  authDomain: "xyz",
+  projectId: "xyz",
+  storageBucket: "xyz",
+  messagingSenderId: "xyz",
   appId: "1:1075844318135:web:eed425262388e7b7ec9e32",
-  measurementId: "G-6BW1P79JSZ"
+  measurementId: "xyz"
 };
 
 // Initialize Firebase
